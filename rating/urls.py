2fbb2feb8f60eb21e3b013de2c591629c6f54c1b
mrZizik <mrfreezzzer@mail.ru>
2017-03-21 19:00:52 +0300

    1603170002

--- rating/urls.py
+++ rating/urls.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 from django.conf.urls import patterns, url
 
-from rating.views import ArticleListView, ArticleDetailView, like, dislike
+from rating.views import ArticleListView, ArticleDetailView, like, dislike, post_comment
 from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/$', ArticleDetailView.as_view(), name='detail'),
     url(r'^like/(?P<object_id>\d+)/$', like, name="like"),
     url(r'^dislike/(?P<object_id>\d+)/$', dislike, name="dislike"),
+    url(r'^comment/(?P<article_id>\d+)/$', post_comment, name="post_comment"),
+    url(r'^comment/(?P<article_id>\d+)/(?P<parent_id>\d+)/$', post_comment, name="post_comment"),
     url(r'^accounts/login/$',  login, {'template_name': 'rating/login.html'}, name='login'),
     url(r'^accounts/logout/$', logout, {'next_page': '/'}, name='logout'),
 )
